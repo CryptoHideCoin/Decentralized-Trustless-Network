cdc5063f9e3d099c785514482222000f7d183ec6
Salvo Maugeri <cryptohidecoin@gmail.com>
2022-01-23 21:10:26 +0100

    Create chccli Command

---
+++ Source/Client/CHCCli/Basic/Common.vb
@@ -0,0 +1,46 @@
+﻿Option Compare Text
+Option Explicit On
+
+
+
+
+
+Namespace AreaCommon
+
+    ''' <summary>
+    ''' This static class run the application
+    ''' </summary>
+    Module Common
+
+        ''' <summary>
+        ''' This method provide to close this application
+        ''' </summary>
+        ''' <param name="message"></param>
+        Sub CloseApplication(ByVal message As String)
+            Console.WriteLine("Error: " & message)
+
+            End
+        End Sub
+
+        ''' <summary>
+        ''' This method provide to run a application
+        ''' </summary>
+        Sub Main()
+            Dim commandManager As New CommandProcessor
+
+            commandManager.command = (New CommandLineDecoder).run()
+
+            'If commandListManager.existCommand(CommandEnumeration.help) Then
+
+            ' I check if by chance the setEnvironmentPath setting is among the commands
+
+            ' if not there I look for the dataPath path
+
+            'End If
+
+            commandManager.execute()
+        End Sub
+
+    End Module
+
+End Namespace
---
+++ Source/Client/CHCCli/General/CommandProcessor.vb
@@ -0,0 +1,59 @@
+﻿Option Compare Text
+Option Explicit On
+
+
+
+
+
+Namespace AreaCommon
+
+    Public Enum CommandEnumeration
+        undefined
+        release
+        help
+    End Enum
+
+    Public Class CommandProcessor
+
+        Public command As New CommandArguments
+
+        ''' <summary>
+        ''' This method provide to execute a release command
+        ''' </summary>
+        ''' <returns></returns>
+        Private Function executeRelease() As Boolean
+            Console.Write(My.Application.Info.Version.ToString())
+
+            Return True
+        End Function
+
+
+        ''' <summary>
+        ''' This method provide to decode into command enumeration
+        ''' </summary>
+        ''' <param name="key"></param>
+        ''' <returns></returns>
+        Public Shared Function getCommandCode(ByVal key As String) As CommandEnumeration
+            Select Case key.Trim()
+                Case "release" : Return CommandEnumeration.release
+                Case "help" : Return CommandEnumeration.help
+                Case Else : Return CommandEnumeration.undefined
+            End Select
+        End Function
+
+        ''' <summary>
+        ''' This method provide to execute a command
+        ''' </summary>
+        ''' <returns></returns>
+        Public Function execute() As Boolean
+            Select Case command.code
+                Case CommandEnumeration.undefined : Console.WriteLine()
+                Case CommandEnumeration.release : Return executeRelease()
+            End Select
+
+            Return True
+        End Function
+
+    End Class
+
+End Namespace
