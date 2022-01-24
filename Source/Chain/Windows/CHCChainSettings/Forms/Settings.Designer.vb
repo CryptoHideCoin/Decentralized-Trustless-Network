525585637802a9fdd9391bf441e314ea71cc74ab
Salvo Maugeri <cryptohidecoin@gmail.com>
2022-01-24 22:08:11 +0100

    Add Chain Settings

---
+++ Source/Chain/Windows/CHCChainSettings/Forms/Settings.Designer.vb
@@ -0,0 +1,366 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Settings
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Settings))
+        Me.tabControl = New System.Windows.Forms.TabControl()
+        Me.tabMain = New System.Windows.Forms.TabPage()
+        Me.serviceIDText = New System.Windows.Forms.TextBox()
+        Me.serviceUUID = New System.Windows.Forms.Label()
+        Me.selectServicePort = New CHCSupportUIControls.SelectPort()
+        Me.selectPublicPort = New CHCSupportUIControls.SelectPort()
+        Me.certificateClient = New CHCSupportUIControls.Certificate()
+        Me.adminWalletAddress = New CHCSupportUIControls.WalletAddress()
+        Me.networkName = New System.Windows.Forms.TextBox()
+        Me.networkNameLabel = New System.Windows.Forms.Label()
+        Me.intranetMode = New System.Windows.Forms.CheckBox()
+        Me.tabInternal = New System.Windows.Forms.TabPage()
+        Me.logGroup = New System.Windows.Forms.GroupBox()
+        Me.logConfiguration = New CHCSupportUIControls.LogControl()
+        Me.useEventRegistry = New System.Windows.Forms.CheckBox()
+        Me.generalFrame = New System.Windows.Forms.GroupBox()
+        Me.loadSettingButton = New System.Windows.Forms.Button()
+        Me.chainServiceName = New System.Windows.Forms.ComboBox()
+        Me.chainSettingLabel = New System.Windows.Forms.Label()
+        Me.localDataLabel = New System.Windows.Forms.Label()
+        Me.browseLocalPath = New System.Windows.Forms.Button()
+        Me.dataPath = New System.Windows.Forms.TextBox()
+        Me.saveButton = New System.Windows.Forms.Button()
+        Me.tabControl.SuspendLayout()
+        Me.tabMain.SuspendLayout()
+        Me.tabInternal.SuspendLayout()
+        Me.logGroup.SuspendLayout()
+        Me.generalFrame.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tabControl
+        '
+        Me.tabControl.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tabControl.Controls.Add(Me.tabMain)
+        Me.tabControl.Controls.Add(Me.tabInternal)
+        Me.tabControl.Location = New System.Drawing.Point(16, 127)
+        Me.tabControl.Name = "tabControl"
+        Me.tabControl.SelectedIndex = 0
+        Me.tabControl.Size = New System.Drawing.Size(709, 286)
+        Me.tabControl.TabIndex = 1
+        '
+        'tabMain
+        '
+        Me.tabMain.Controls.Add(Me.serviceIDText)
+        Me.tabMain.Controls.Add(Me.serviceUUID)
+        Me.tabMain.Controls.Add(Me.selectServicePort)
+        Me.tabMain.Controls.Add(Me.selectPublicPort)
+        Me.tabMain.Controls.Add(Me.certificateClient)
+        Me.tabMain.Controls.Add(Me.adminWalletAddress)
+        Me.tabMain.Controls.Add(Me.networkName)
+        Me.tabMain.Controls.Add(Me.networkNameLabel)
+        Me.tabMain.Controls.Add(Me.intranetMode)
+        Me.tabMain.Location = New System.Drawing.Point(4, 22)
+        Me.tabMain.Name = "tabMain"
+        Me.tabMain.Padding = New System.Windows.Forms.Padding(3)
+        Me.tabMain.Size = New System.Drawing.Size(701, 260)
+        Me.tabMain.TabIndex = 0
+        Me.tabMain.Text = "Main"
+        Me.tabMain.UseVisualStyleBackColor = True
+        '
+        'serviceIDText
+        '
+        Me.serviceIDText.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.serviceIDText.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.serviceIDText.Location = New System.Drawing.Point(156, 52)
+        Me.serviceIDText.Name = "serviceIDText"
+        Me.serviceIDText.Size = New System.Drawing.Size(520, 21)
+        Me.serviceIDText.TabIndex = 1
+        '
+        'serviceUUID
+        '
+        Me.serviceUUID.AutoSize = True
+        Me.serviceUUID.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.serviceUUID.Location = New System.Drawing.Point(82, 55)
+        Me.serviceUUID.Name = "serviceUUID"
+        Me.serviceUUID.Size = New System.Drawing.Size(68, 13)
+        Me.serviceUUID.TabIndex = 34
+        Me.serviceUUID.Text = "Service ID"
+        '
+        'selectServicePort
+        '
+        Me.selectServicePort.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.selectServicePort.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.selectServicePort.label = "Service port number (0..65535)"
+        Me.selectServicePort.Location = New System.Drawing.Point(396, 201)
+        Me.selectServicePort.Name = "selectServicePort"
+        Me.selectServicePort.Size = New System.Drawing.Size(282, 23)
+        Me.selectServicePort.TabIndex = 5
+        Me.selectServicePort.value = 0
+        '
+        'selectPublicPort
+        '
+        Me.selectPublicPort.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.selectPublicPort.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.selectPublicPort.label = "Public port number (0..65535)"
+        Me.selectPublicPort.Location = New System.Drawing.Point(396, 172)
+        Me.selectPublicPort.Name = "selectPublicPort"
+        Me.selectPublicPort.Size = New System.Drawing.Size(282, 23)
+        Me.selectPublicPort.TabIndex = 4
+        Me.selectPublicPort.value = 0
+        '
+        'certificateClient
+        '
+        Me.certificateClient.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.certificateClient.dataPath = ""
+        Me.certificateClient.Location = New System.Drawing.Point(81, 136)
+        Me.certificateClient.Name = "certificateClient"
+        Me.certificateClient.noChange = True
+        Me.certificateClient.serviceId = ""
+        Me.certificateClient.Size = New System.Drawing.Size(597, 30)
+        Me.certificateClient.TabIndex = 3
+        Me.certificateClient.urlService = ""
+        Me.certificateClient.value = ""
+        '
+        'adminWalletAddress
+        '
+        Me.adminWalletAddress.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.adminWalletAddress.caption = "Admin wallet address"
+        Me.adminWalletAddress.dataPath = ""
+        Me.adminWalletAddress.Location = New System.Drawing.Point(17, 79)
+        Me.adminWalletAddress.Name = "adminWalletAddress"
+        Me.adminWalletAddress.Size = New System.Drawing.Size(662, 51)
+        Me.adminWalletAddress.TabIndex = 2
+        Me.adminWalletAddress.value = ""
+        '
+        'networkName
+        '
+        Me.networkName.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.networkName.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.networkName.Location = New System.Drawing.Point(156, 25)
+        Me.networkName.Name = "networkName"
+        Me.networkName.Size = New System.Drawing.Size(520, 21)
+        Me.networkName.TabIndex = 0
+        '
+        'networkNameLabel
+        '
+        Me.networkNameLabel.AutoSize = True
+        Me.networkNameLabel.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.networkNameLabel.Location = New System.Drawing.Point(60, 28)
+        Me.networkNameLabel.Name = "networkNameLabel"
+        Me.networkNameLabel.Size = New System.Drawing.Size(90, 13)
+        Me.networkNameLabel.TabIndex = 27
+        Me.networkNameLabel.Text = "Network name"
+        '
+        'intranetMode
+        '
+        Me.intranetMode.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.intranetMode.AutoSize = True
+        Me.intranetMode.CheckAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.intranetMode.Checked = True
+        Me.intranetMode.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.intranetMode.Location = New System.Drawing.Point(568, 234)
+        Me.intranetMode.Name = "intranetMode"
+        Me.intranetMode.Size = New System.Drawing.Size(108, 17)
+        Me.intranetMode.TabIndex = 6
+        Me.intranetMode.Text = "Intranet mode"
+        Me.intranetMode.UseVisualStyleBackColor = True
+        '
+        'tabInternal
+        '
+        Me.tabInternal.Controls.Add(Me.logGroup)
+        Me.tabInternal.Controls.Add(Me.useEventRegistry)
+        Me.tabInternal.Location = New System.Drawing.Point(4, 22)
+        Me.tabInternal.Name = "tabInternal"
+        Me.tabInternal.Size = New System.Drawing.Size(701, 260)
+        Me.tabInternal.TabIndex = 2
+        Me.tabInternal.Text = "Internal"
+        Me.tabInternal.UseVisualStyleBackColor = True
+        '
+        'logGroup
+        '
+        Me.logGroup.Controls.Add(Me.logConfiguration)
+        Me.logGroup.Location = New System.Drawing.Point(13, 15)
+        Me.logGroup.Name = "logGroup"
+        Me.logGroup.Size = New System.Drawing.Size(675, 142)
+        Me.logGroup.TabIndex = 0
+        Me.logGroup.TabStop = False
+        Me.logGroup.Text = "Log"
+        '
+        'logConfiguration
+        '
+        Me.logConfiguration.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.logConfiguration.Location = New System.Drawing.Point(6, 20)
+        Me.logConfiguration.Name = "logConfiguration"
+        Me.logConfiguration.Size = New System.Drawing.Size(669, 105)
+        Me.logConfiguration.TabIndex = 11
+        Me.logConfiguration.trackConfiguration = CHCCommonLibrary.Support.LogEngine.TrackRuntimeModeEnum.dontTrackEver
+        Me.logConfiguration.trackRotateFrequency = CHCCommonLibrary.Support.LogRotateEngine.LogRotateConfig.FrequencyEnum.every12h
+        Me.logConfiguration.trackRotateKeepFile = CHCCommonLibrary.Support.LogRotateEngine.LogRotateConfig.KeepFileEnum.nothingFiles
+        Me.logConfiguration.trackRotateKeepLast = CHCCommonLibrary.Support.LogRotateEngine.LogRotateConfig.KeepEnum.lastDay
+        Me.logConfiguration.useTrackRotate = False
+        '
+        'useEventRegistry
+        '
+        Me.useEventRegistry.AutoSize = True
+        Me.useEventRegistry.Checked = True
+        Me.useEventRegistry.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.useEventRegistry.Location = New System.Drawing.Point(13, 173)
+        Me.useEventRegistry.Name = "useEventRegistry"
+        Me.useEventRegistry.Size = New System.Drawing.Size(134, 17)
+        Me.useEventRegistry.TabIndex = 1
+        Me.useEventRegistry.Text = "Use Event Registry"
+        Me.useEventRegistry.UseVisualStyleBackColor = True
+        '
+        'generalFrame
+        '
+        Me.generalFrame.Controls.Add(Me.localDataLabel)
+        Me.generalFrame.Controls.Add(Me.browseLocalPath)
+        Me.generalFrame.Controls.Add(Me.dataPath)
+        Me.generalFrame.Controls.Add(Me.loadSettingButton)
+        Me.generalFrame.Controls.Add(Me.chainServiceName)
+        Me.generalFrame.Controls.Add(Me.chainSettingLabel)
+        Me.generalFrame.Location = New System.Drawing.Point(16, 13)
+        Me.generalFrame.Name = "generalFrame"
+        Me.generalFrame.Size = New System.Drawing.Size(705, 108)
+        Me.generalFrame.TabIndex = 0
+        Me.generalFrame.TabStop = False
+        '
+        'loadSettingButton
+        '
+        Me.loadSettingButton.Location = New System.Drawing.Point(624, 74)
+        Me.loadSettingButton.Name = "loadSettingButton"
+        Me.loadSettingButton.Size = New System.Drawing.Size(75, 23)
+        Me.loadSettingButton.TabIndex = 3
+        Me.loadSettingButton.Text = "Load"
+        Me.loadSettingButton.UseVisualStyleBackColor = True
+        '
+        'chainServiceName
+        '
+        Me.chainServiceName.FormattingEnabled = True
+        Me.chainServiceName.Items.AddRange(New Object() {"Primary"})
+        Me.chainServiceName.Location = New System.Drawing.Point(75, 20)
+        Me.chainServiceName.Name = "chainServiceName"
+        Me.chainServiceName.Size = New System.Drawing.Size(583, 21)
+        Me.chainServiceName.TabIndex = 0
+        '
+        'chainSettingLabel
+        '
+        Me.chainSettingLabel.AutoSize = True
+        Me.chainSettingLabel.Location = New System.Drawing.Point(29, 23)
+        Me.chainSettingLabel.Name = "chainSettingLabel"
+        Me.chainSettingLabel.Size = New System.Drawing.Size(40, 13)
+        Me.chainSettingLabel.TabIndex = 3
+        Me.chainSettingLabel.Text = "Chain"
+        '
+        'localDataLabel
+        '
+        Me.localDataLabel.AutoSize = True
+        Me.localDataLabel.Location = New System.Drawing.Point(6, 50)
+        Me.localDataLabel.Name = "localDataLabel"
+        Me.localDataLabel.Size = New System.Drawing.Size(63, 13)
+        Me.localDataLabel.TabIndex = 24
+        Me.localDataLabel.Text = "Data path"
+        '
+        'browseLocalPath
+        '
+        Me.browseLocalPath.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.browseLocalPath.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.browseLocalPath.Location = New System.Drawing.Point(664, 46)
+        Me.browseLocalPath.Name = "browseLocalPath"
+        Me.browseLocalPath.Size = New System.Drawing.Size(35, 23)
+        Me.browseLocalPath.TabIndex = 2
+        Me.browseLocalPath.Text = "..."
+        Me.browseLocalPath.UseVisualStyleBackColor = True
+        '
+        'dataPath
+        '
+        Me.dataPath.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dataPath.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dataPath.Location = New System.Drawing.Point(75, 47)
+        Me.dataPath.Name = "dataPath"
+        Me.dataPath.Size = New System.Drawing.Size(583, 21)
+        Me.dataPath.TabIndex = 1
+        '
+        'saveButton
+        '
+        Me.saveButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.saveButton.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.saveButton.Location = New System.Drawing.Point(732, 19)
+        Me.saveButton.Name = "saveButton"
+        Me.saveButton.Size = New System.Drawing.Size(106, 49)
+        Me.saveButton.TabIndex = 5
+        Me.saveButton.Text = "Save"
+        Me.saveButton.UseVisualStyleBackColor = True
+        '
+        'Settings
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(850, 424)
+        Me.Controls.Add(Me.saveButton)
+        Me.Controls.Add(Me.generalFrame)
+        Me.Controls.Add(Me.tabControl)
+        Me.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Settings"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Chain Service Settings - Crypto Hide Coin DTN"
+        Me.tabControl.ResumeLayout(False)
+        Me.tabMain.ResumeLayout(False)
+        Me.tabMain.PerformLayout()
+        Me.tabInternal.ResumeLayout(False)
+        Me.tabInternal.PerformLayout()
+        Me.logGroup.ResumeLayout(False)
+        Me.generalFrame.ResumeLayout(False)
+        Me.generalFrame.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents tabControl As TabControl
+    Friend WithEvents tabMain As TabPage
+    Friend WithEvents serviceIDText As TextBox
+    Friend WithEvents serviceUUID As Label
+    Friend WithEvents selectServicePort As CHCSupportUIControls.SelectPort
+    Friend WithEvents selectPublicPort As CHCSupportUIControls.SelectPort
+    Friend WithEvents certificateClient As CHCSupportUIControls.Certificate
+    Friend WithEvents adminWalletAddress As CHCSupportUIControls.WalletAddress
+    Friend WithEvents networkName As TextBox
+    Friend WithEvents networkNameLabel As Label
+    Friend WithEvents intranetMode As CheckBox
+    Friend WithEvents tabInternal As TabPage
+    Friend WithEvents logGroup As GroupBox
+    Friend WithEvents logConfiguration As CHCSupportUIControls.LogControl
+    Friend WithEvents useEventRegistry As CheckBox
+    Friend WithEvents generalFrame As GroupBox
+    Friend WithEvents localDataLabel As Label
+    Friend WithEvents browseLocalPath As Button
+    Friend WithEvents dataPath As TextBox
+    Friend WithEvents loadSettingButton As Button
+    Friend WithEvents chainServiceName As ComboBox
+    Friend WithEvents chainSettingLabel As Label
+    Friend WithEvents saveButton As Button
+End Class
