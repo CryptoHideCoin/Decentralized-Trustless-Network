8424ecbb044043b362199500461bf50443f4c683
Salvo Maugeri <cryptohidecoin@gmail.com>
2022-01-30 23:05:21 +0100

    Scaffolding CHC Console Log

---
+++ Source/Client/CHC Console Log/Basic/Common.vb
@@ -0,0 +1,34 @@
+﻿Option Compare Text
+Option Explicit On
+
+
+
+
+
+Namespace AreaCommon
+
+    ''' <summary>
+    ''' This static class run the application
+    ''' </summary>
+    Module Common
+
+        ''' <summary>
+        ''' This method provide to close this application
+        ''' </summary>
+        ''' <param name="message"></param>
+        Sub CloseApplication(ByVal message As String)
+            Console.WriteLine("Error: " & message)
+
+            End
+        End Sub
+
+        ''' <summary>
+        ''' This method provide to run a application
+        ''' </summary>
+        Sub Main()
+
+        End Sub
+
+    End Module
+
+End Namespace
